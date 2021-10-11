a9888b06d8674db393e82b1e485d112804801b64
JY-DEV <kjy0302014@gmail.com>
2021-10-11 19:18:59 +0900

    DecimalUtilTest 분리

--- util/src/test/java/com/jydev/util/DecimalUtilTest.kt
+++ util/src/test/java/com/jydev/util/DecimalUtilTest.kt
@@ -1,6 +1,5 @@
-package com.jydev.jobplanetandroid
+package com.jydev.util
 
-import com.jydev.util.getCommaDecimal
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
